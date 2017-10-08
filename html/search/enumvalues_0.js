fd26d7aaab1fa66c9177c5c384211f0876a70fa3
carlos-caronte <carlos.caronte@gmail.com>
2017-10-08 17:42:51 +0200

    doc update

--- html/search/enumvalues_0.js
+++ html/search/enumvalues_0.js
@@ -4,6 +4,7 @@ var searchData=
   ['v_5ferr_5ffile',['V_ERR_FILE',['../vector_8h.html#a8f5f2d49a722b54d5290baae5f9b6e6ba73a550dfdd1f88916839e9aff4487e8c',1,'vector.h']]],
   ['v_5ferr_5finvalid_5fargument',['V_ERR_INVALID_ARGUMENT',['../vector_8h.html#a8f5f2d49a722b54d5290baae5f9b6e6bafd334db1a1df8eaadce345c8cb89eb35',1,'vector.h']]],
   ['v_5ferr_5fis_5fempty',['V_ERR_IS_EMPTY',['../vector_8h.html#a8f5f2d49a722b54d5290baae5f9b6e6bad52e5a58e1b25b68d1d9b14a1afbfd52',1,'vector.h']]],
+  ['v_5ferr_5fmax_5fcapacity',['V_ERR_MAX_CAPACITY',['../vector_8h.html#a8f5f2d49a722b54d5290baae5f9b6e6ba1c1eaea785d3de6bcf533b2f79ddf797',1,'vector.h']]],
   ['v_5ferr_5fmemcpy',['V_ERR_MEMCPY',['../vector_8h.html#a8f5f2d49a722b54d5290baae5f9b6e6ba6435d41bd12096e07850be3ce3431f77',1,'vector.h']]],
   ['v_5ferr_5fmemmove',['V_ERR_MEMMOVE',['../vector_8h.html#a8f5f2d49a722b54d5290baae5f9b6e6ba09edc65ff18a938948c454d897f6d567',1,'vector.h']]],
   ['v_5ferr_5fout_5fof_5frange',['V_ERR_OUT_OF_RANGE',['../vector_8h.html#a8f5f2d49a722b54d5290baae5f9b6e6ba54e0bb4785b3b69e90d8976dd1005741',1,'vector.h']]],
